dc0e70cae0fe434ef3866612ee4e9b82b456364a
Isis Ebsen <root@5ht2.me>
2023-09-03 16:22:22 +0530

    add license

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,9 @@
-use std::{fmt::Display, fs::read_to_string, net::SocketAddr};
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use serde::Deserialize;
+use std::{fmt::Display, fs::read_to_string, net::SocketAddr};
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct Config {
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #![forbid(unsafe_code)]
 #![deny(clippy::all, clippy::pedantic, clippy::nursery)]
 
